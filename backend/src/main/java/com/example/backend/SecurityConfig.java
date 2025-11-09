0d2fd3189da8d6bac3475e386bf0cdd0e42f6e69
jose5330 <christoj5330@gmail.com>
2025-11-09 10:59:10 +1030

    added frontend endpoint to backend

--- backend/src/main/java/com/example/backend/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // frontend
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://connecthub-g1qb.onrender.com")); // frontend
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of("*")); // allow all headers
